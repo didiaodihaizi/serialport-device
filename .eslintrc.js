9c41a67218ea7b68111dae0564ecf70d07ec8366
徐志豪 <xuzhihao@advanpro.hk>
2017-12-12 09:35:17 +0800

    modified

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: 'standard',
+  globals: {
+    __static: true
+  },
+  plugins: [
+    'html'
+  ],
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    'import/first': 0,
+    "quotes": [2, "single", "avoid-escape"],
+    'import/no-webpack-loader-syntax': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}
--- app/src/renderer/main.js
+++ app/src/renderer/main.js
@@ -1,5 +1,4 @@
 import Vue from 'vue'
-import SWorker from 'simple-web-worker'
 import axios from 'axios'
 import iView from 'iview'
 import 'iview/dist/styles/iview.css'
@@ -8,15 +7,11 @@ import VueECharts from 'vue-echarts'
 import App from './App'
 import router from './router'
 import store from './store'
-import lodash from 'lodash'
-import away from 'away'
 
 Vue.http = Vue.prototype.$http = axios
 Vue.component('chart', VueECharts)
 Vue.config.productionTip = false
 Vue.prototype.$echarts = Echarts
-Vue.prototype.$SWorker = SWorker
-Vue.prototype.$lodash = lodash
 Vue.use(iView)
 
 /* eslint-disable no-new */
